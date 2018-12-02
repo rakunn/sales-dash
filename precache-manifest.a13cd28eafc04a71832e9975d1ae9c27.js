17eeffa449d0f47a9ed2ac15e33d82e356a46076
Rafal.Bagrowski <rafal.bagrowski@eu.jll.com>
2018-12-02 13:10:45 +0100

    Updates

---
+++ precache-manifest.a13cd28eafc04a71832e9975d1ae9c27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "838cf464dfc89702e965",
+    "url": "/sales-dash/static/js/runtime~main.838cf464.js"
+  },
+  {
+    "revision": "87637c8e7d0fd9b334f9",
+    "url": "/sales-dash/static/js/main.87637c8e.chunk.js"
+  },
+  {
+    "revision": "1b21e3f4f476f95bbea4",
+    "url": "/sales-dash/static/js/1.1b21e3f4.chunk.js"
+  },
+  {
+    "revision": "87637c8e7d0fd9b334f9",
+    "url": "/sales-dash/static/css/main.70c8c3e9.chunk.css"
+  },
+  {
+    "revision": "7f87896489674bdf66c3dbaf935b2c36",
+    "url": "/sales-dash/index.html"
+  }
+];
